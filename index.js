7dfe081dc41a46f03b71d4cec4c5cf48c69885c0
CHORU-OFFICIAL <112081055+ChoruOfficial@users.noreply.github.com>
2025-09-09 13:20:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express();
 app.get('/', (req, res) => res.send('Hello from Costaños!'));
 
 // Non-root port 8080
-app.listen(8080, '0.0.0.0', () => console.log('Server running on http://0.0.0.0:8080'));
+app.listen(8080, () => console.log('Server running on http://localhost:8080'));
